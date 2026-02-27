49def3db28187c2ff7a8b658ad1e5d1672e91e69
bakkerb <bjornbakker1989@gmail.com>
2026-02-27 23:10:18 +0100

    added asteroids spawning; working on collisions

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -15,9 +17,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
     
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
@@ -26,8 +33,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        for thing in updatable:
-            thing.update(dt)
+        updatable.update(dt)
         screen.fill("black")
         for thing in drawable:
             thing.draw(screen)
